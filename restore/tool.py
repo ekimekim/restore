914d173f5fcb42a3e09ff873920588321b798a18
Mike Lang <mikelang3000@gmail.com>
2014-11-23 07:02:29 -0800

    Add match, prune cli commands

--- restore/tool.py
+++ restore/tool.py
@@ -17,6 +17,24 @@ def add(manifest, *path, **kwargs):
 		manifest.add_file_tree(p)
 	manifest.savefile(manifest_path)
 
+@cli
+def prune(manifest):
+	"""Remove files from manifest that no longer exist"""
+	manifest_path = manifest
+	manifest = Manifest(manifest_path)
+	for path in manifest.files.keys():
+		if not os.path.exists(path):
+			del manifest.files[path]
+	manifest.savefile(manifest_path)
+
+@cli
+def match(manifest):
+	"""Automatically find matching handlers for all unhandled files in manifest"""
+	manifest_path = manifest
+	manifest = Manifest(manifest_path)
+	manifest.find_matches()
+	manifest.savefile(manifest_path)
+
 
 if __name__ == '__main__':
 	cli()
