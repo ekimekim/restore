da22df355b469c36a1240bf0404d4131f8b765f8
Mike Lang <mikelang3000@gmail.com>
2014-11-23 04:03:17 -0800

    Seperate default handlers into more useful segments

--- restore/handlers/__init__.py
+++ restore/handlers/__init__.py
@@ -2,10 +2,19 @@
 import basics
 import packages
 
-DEFAULT_HANDLERS = [
+# first and last handlers that should always be in those positions to ensure proper operation
+# these cover things like restoring whole directories and fallback "just back up the file" stuff
+FIRST_HANDLERS = [
 	basics.HandledByParent,
-	packages.PacmanHandler,
+]
+
+LAST_HANDLERS = [
 	basics.SymbolicLinkHandler,
 	basics.DirectoryHandler,
 	basics.FileHandler,
 ]
+
+DEFAULT_HANDLERS = [
+	packages.PacmanHandler,
+]
+DEFAULT_HANDLERS = FIRST_HANDLERS + DEFAULT_HANDLERS + LAST_HANDLERS
