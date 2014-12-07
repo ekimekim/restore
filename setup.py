0b3d3bd482f762208ebd1f1c4152e5b90cd4da05
Mike Lang <mikelang3000@gmail.com>
2014-12-07 05:44:07 -0800

    Fix setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,8 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
 	name='restore',
 	description='Application to assist in backing up and restoring highly-recoverable data',
-	requires=['gevent>=1.0', 'argh'],
+	requires=['gevent(>=1.0)', 'argh'],
+	packages=find_packages(),
 )
