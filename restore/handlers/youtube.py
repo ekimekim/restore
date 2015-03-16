59ce1e9bf2318b4e66c1a2df587a4eaadfdbaad5
Mike Lang <mikelang3000@gmail.com>
2015-03-16 08:58:06 -0700

    youtube: Recognise and restore start:end times
    NEEDS RESTORE TEST

--- restore/handlers/youtube.py
+++ restore/handlers/youtube.py
@@ -1,6 +1,7 @@
 
 import os
 import string
+from uuid import uuid4
 
 import easycmd
 
@@ -32,11 +33,22 @@ class YoutubeHandler(SavesFileInfo):
 
 	@classmethod
 	def splitname(cls, filepath):
-		"""Returns (filename, title, id, ext) if filename is formatted correctly."""
+		"""Returns (filename, title, id, (start, end), ext) if filename is formatted correctly.
+		start/end may be None."""
 		error = lambda msg: ValueError("Bad filename: {}".format(msg))
 		filename = os.path.basename(filepath)
+		start, end = None, None
+		# find extension
 		name, ext = os.path.splitext(filename)
 		ext = ext[1:] if ext else None # splitext returns with a leading '.' normally, or '' if no ext
+		# look for optional ".START:END" at end of name
+		name_parts = name.split('.')
+		time_info = name_parts[-1].split(':')
+		if len(time_info) == 2 and all(part.isdigit() for part in time_info if part):
+			start, end = [int(part) if part else None for part in time_info]
+			# it's valid - assign remaining name_parts back to name
+			name = '.'.join(name_parts[:-1])
+		# look for id
 		if len(name) < 12:
 			raise error("too short")
 		title, dash, id = name[:-12], name[-12], name[-11:]
@@ -45,7 +57,7 @@ class YoutubeHandler(SavesFileInfo):
 		ALLOWED = set(string.letters + string.digits + '_-')
 		if any(c not in ALLOWED for c in id):
 			raise error("no id")
-		return filename, title, id, ext
+		return filename, title, id, (start, end), ext
 
 	@classmethod
 	def verify_title(cls, id, title):
@@ -72,26 +84,60 @@ class YoutubeHandler(SavesFileInfo):
 	@classmethod
 	def match(cls, manifest, filepath):
 		try:
-			filename, title, id, ext = cls.splitname(filepath)
+			filename, title, id, (start, end), ext = cls.splitname(filepath)
 		except ValueError:
 			return
 		if not cls.verify_title(id, title):
 			return
-		return (id,), {'format': ext}
+		args = (id,)
+		kwargs = dict(format=ext)
+		if start is not None:
+			kwargs['start'] = start
+		if end is not None:
+			kwargs['end'] = end
+		return args, kwargs
 
-	def __init__(self, manifest, filepath, id, format=None):
+	def __init__(self, manifest, filepath, id, format=None, start=None, end=None):
 		super(YoutubeHandler, self).__init__(manifest, filepath)
 		self.id = id
 		self.format = format
+		self.time_info = [int(part) if part else None for part in (start, end)]
 
 	def get_depends(self):
 		return super(YoutubeHandler, self).get_depends() | {'/usr/bin/youtube-dl'}
 
 	def get_args(self):
-		return (self.id,), {'format': self.format}
+		args = (self.id,)
+		kwargs = dict(format=self.format)
+		start, end = self.time_info
+		if start is not None:
+			kwargs['start'] = start
+		if end is not None:
+			kwargs['end'] = end
+		return args, kwargs
 
 	def restore(self, extra_data):
-		args = [self.id, '--verbose', '--no-progress', '--output', self.filepath]
+		if self.time_info == (None, None):
+			self.download_to_path(self.filepath)
+		else:
+			start, end = self.time_info
+			time_args = []
+			if start is not None:
+				time_args += ['-ss', start]
+			if end is not None:
+				interval = end if start is None else end - start
+				time_args += ['-t', interval]
+			# save to tmp file, then use ffmpeg to trim
+			tmp_path = "/tmp/{}.{}".format(str(uuid4()), self.format)
+			try:
+				self.download_to_path(tmp_path)
+				easycmd.cmd(['ffmpeg', '-y'] + time_args + ['-i', tmp_path, '-strict', '-2', self.filepath])
+			finally:
+				os.remove(tmp_path)
+		super(YoutubeHandler, self).restore(extra_data)
+
+	def download_to_path(self, filepath):
+		args = [self.id, '--verbose', '--no-progress', '--output', filepath]
 		if self.format in self.AUDIO_EXTENSIONS:
 			args += ['--format', 'bestaudio', '--extract-audio', '--audio-format', self.AUDIO_EXTENSIONS[self.format]]
 		elif self.format in self.VIDEO_EXTENSIONS:
@@ -101,5 +147,4 @@ class YoutubeHandler(SavesFileInfo):
 		else:
 			raise ValueError("Unknown file extension: {}".format(self.format))
 		self.youtube_dl(*args, exc=True)
-		assert os.path.exists(self.filepath), "youtube-dl returned succesfully but file does not exist"
-		super(YoutubeHandler, self).restore(extra_data)
+		assert os.path.exists(filepath), "youtube-dl returned succesfully but file does not exist"
