8a2b9ab29e969f2a2acb4f4b8f90668d45a16b25
Mike Lang <mikelang3000@gmail.com>
2015-05-10 00:21:17 -0700

    ignore handler: Allow user to ignore paths by setting env vars

--- restore/handlers/ignore.py
+++ restore/handlers/ignore.py
@@ -1,11 +1,15 @@
 
 import os
+import re
 
 from restore.handler import Handler
 
 
 class IgnoreHandler(Handler):
-	"""A handler that does not re-create the file, for temporary files and others you do not wish to save."""
+	"""A handler that does not re-create the file, for temporary files and others you do not wish to save.
+	Automatically matches on certain file extensions and hard-coded paths.
+	Specify additional paths to match using env var MATCH_IGNORE (seperate paths with : and escape : as \: )
+	"""
 
 	name = 'ignore'
 	restores_contents = True
@@ -18,6 +22,10 @@ class IgnoreHandler(Handler):
 		'/tmp', '/run', '/var/tmp', '/var/run', '/var/lock',
 		'/var/cache',
 	}
+	if 'MATCH_IGNORE' in os.environ:
+		_paths = re.split(r'(?<!\\):', os.environ['MATCH_IGNORE'])
+		_paths = [_path.replace(r"\:", ":") for _path in _paths]
+		MATCH_PATHS |= set(_paths)
 
 	@classmethod
 	def match(cls, manifest, filepath):
